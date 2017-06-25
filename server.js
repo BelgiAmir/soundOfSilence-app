f28fa5427d39218ea3bd245a1a14aa3fa27c8909
Belgi Amir <BelgiAmir251@gmail.com>
2017-06-25 22:35:38 +0300

    try

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(path.join(__dirname, '/')));
 // app.use(express.static(static_dir));
 // Set our api routes
 app.use('/api', api);
