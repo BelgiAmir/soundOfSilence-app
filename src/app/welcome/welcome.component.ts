5cbd68e014a26615480ec8c9b55527c1ae4071fa
Belgi Amir <BelgiAmir251@gmail.com>
2017-07-14 14:13:42 +0300

    k

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SharedService } from '../shared/shared.service';
 import { InstructionsService } from '../instructions/instructions.service';
+import { SlidesService } from '../slides/slides.service';
+import { Slide } from '../slides/slide';
 
 @Component({
   selector: 'app-welcome',
@@ -9,14 +11,21 @@ import { InstructionsService } from '../instructions/instructions.service';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
-
+  slides: Slide[];
+  errorMessage: string;
   constructor(private router: Router, private _sharedSrv: SharedService,
-    private _instructionService: InstructionsService) {
+    private _instructionService: InstructionsService, private _slidesService: SlidesService) {
     _sharedSrv.SetStageTitleAndProgress('ברוכים הבאים', 0);
   }
 
   ngOnInit() {
+    this._slidesService.getSlides()
+      .subscribe(slides => {
+        this.slides = slides;
+      },
+      error => this.errorMessage = <any>error);
   }
+
   continue() {
     const nextBtn = '\slides';
     const instructionText = 'בשלב זה, עליכם ללמוד 15 צמדים של תמונות ושמות של שירים מוכרים';
